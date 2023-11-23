b266c9a584a468dfa93a5f7855e274faf1bbfb01
Suyash <78982011+Gsuyash20@users.noreply.github.com>
2023-11-23 12:05:15 +0530

    Update kalmanfilter.cpp

--- src/kalmanfilter.cpp
+++ src/kalmanfilter.cpp
@@ -1,8 +1,3 @@
-// ------------------------------------------------------------------------------- //
-// Advanced Kalman Filtering and Sensor Fusion Course - Linear Kalman Filter
-//
-// ####### STUDENT FILE #######
-//
 // Usage:
 // -Rename this file to "kalmanfilter.cpp" if you want to use this code.
 
@@ -13,7 +8,7 @@
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
 constexpr double INIT_POS_STD = 0;
-constexpr double INIT_VEL_STD = 15;
+constexpr double INIT_VEL_STD = 0;
 constexpr double ACCEL_STD = 0.1;
 constexpr double GPS_POS_STD = 3.0;
 // -------------------------------------------------- //
@@ -36,6 +31,13 @@ void KalmanFilter::predictionStep(double dt)
             // Assume the initial position is (X,Y) = (0,0) m
             // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
             state << 0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4);
+            
+            const double init_pos_std = INIT_POS_STD;
+            const double init_vel_std = INIT_VEL_STD;
+            cov(0,0) = INIT_POS_STD*INIT_POS_STD;
+            cov(1,1) = INIT_POS_STD*INIT_POS_STD;
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
 
             setState(state);
             setCovariance(cov);
@@ -52,8 +54,18 @@ void KalmanFilter::predictionStep(double dt)
         // Hint: You can use the constants: ACCEL_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
+        MatrixXd F = Matrix4d();
+        F << 1,0,dt,0,0,1,0,dt,0,0,1,0,0,0,0,1;
+        
+        MatrixXd Q = Matrix2d::Zero();
+        Q(0,0) = (ACCEL_STD*ACCEL_STD);
+        Q(1,1) = (ACCEL_STD*ACCEL_STD);
+        
+        MatrixXd L = MatrixXd(4,2);
+        L << (0.5*dt*dt),0,0,(0.5*dt*dt),dt,0,0,dt;
 
-
+        state = F * state;
+        cov = F * cov * F.transpose() + L * Q * L.transpose();
         // ----------------------------------------------------------------------- //
 
         setState(state);
@@ -123,4 +135,3 @@ VehicleState KalmanFilter::getVehicleState()
 void KalmanFilter::predictionStep(GyroMeasurement gyro, double dt){predictionStep(dt);}
 void KalmanFilter::handleLidarMeasurements(const std::vector<LidarMeasurement>& dataset, const BeaconMap& map){}
 void KalmanFilter::handleLidarMeasurement(LidarMeasurement meas, const BeaconMap& map){}
-
