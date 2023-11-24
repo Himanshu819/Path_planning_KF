c552c04910d24150381c4de64c82b4412b3f0ecf
Suyash <78982011+Gsuyash20@users.noreply.github.com>
2023-11-24 17:24:09 +0530

    Update kalmanfilter_ekf.cpp

--- src/kalmanfilter_ekf.cpp
+++ src/kalmanfilter_ekf.cpp
@@ -1,8 +1,3 @@
-// ------------------------------------------------------------------------------- //
-// Advanced Kalman Filtering and Sensor Fusion Course - Extended Kalman Filter
-//
-// ####### STUDENT FILE #######
-//
 // Usage:
 // -Rename this file to "kalmanfilter.cpp" if you want to use this code.
 
@@ -46,7 +41,35 @@ void KalmanFilter::handleLidarMeasurement(LidarMeasurement meas, const BeaconMap
         BeaconData map_beacon = map.getBeaconWithId(meas.id); // Match Beacon with built in Data Association Id
         if (meas.id != -1 && map_beacon.id != -1)
         {           
-            // The map matched beacon positions can be accessed using: map_beacon.x AND map_beacon.y
+              // Measurement Vector
+            VectorXd z = Vector2d::Zero();
+            z << meas.range, meas.theta;
+
+            // Predicted Measurement Vector (Measurement Model)
+            VectorXd z_hat = Vector2d::Zero();
+            double delta_x = map_beacon.x - state[0];
+            double delta_y = map_beacon.y - state[1];
+            double zhat_range = sqrt(delta_x*delta_x + delta_y*delta_y);
+            double zhat_theta = wrapAngle(atan2(delta_y,delta_x) - state[2]);
+            z_hat << zhat_range, zhat_theta;
+
+            // Measurement Model Sensitivity Matrix
+            MatrixXd H = MatrixXd(2,4);
+            H << -delta_x/zhat_range,-delta_y/zhat_range,0,0,delta_y/zhat_range/zhat_range,-delta_x/zhat_range/zhat_range,-1,0;
+
+            // Generate Measurement Model Noise Covariance Matrix
+            MatrixXd R = Matrix2d::Zero();
+            R(0,0) = LIDAR_RANGE_STD*LIDAR_RANGE_STD;
+            R(1,1) = LIDAR_THETA_STD*LIDAR_THETA_STD;
+
+            VectorXd y = z - z_hat;
+            MatrixXd S = H * cov * H.transpose() + R;
+            MatrixXd K = cov*H.transpose()*S.inverse();
+
+            y(1) = wrapAngle(y(1)); // Wrap the Heading Innovation
+
+            state = state + K*y;
+            cov = (Matrix4d::Identity() - K*H) * cov;   
         }
 
         // ----------------------------------------------------------------------- //
@@ -72,7 +95,28 @@ void KalmanFilter::predictionStep(GyroMeasurement gyro, double dt)
         // values within correct range, otherwise strange angle effects might be seen.
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
+        double x = state(0);
+        double y = state(1);
+        double psi = state(2);
+        double V = state(3);
 
+        // Update State
+        double x_new = x + dt * V * cos(psi);
+        double y_new = y + dt * V * sin(psi);
+        double psi_new = wrapAngle(psi + dt * gyro.psi_dot);
+        double V_new = V;
+        state << x_new,y_new,psi_new,V_new;
+
+        // Generate F Matrix
+        MatrixXd F = Matrix4d::Zero();
+        F << 1,0,-dt*V*sin(psi),dt*cos(psi),0,1,dt*V*cos(psi),dt*sin(psi),0,0,1,0,0,0,0,1;
+
+        // Generate Q Matrix
+        MatrixXd Q = Matrix4d::Zero();
+        Q(2,2) = dt*dt*GYRO_STD*GYRO_STD;
+        Q(3,3) = dt*dt*ACCEL_STD*ACCEL_STD;
+
+        cov = F * cov * F.transpose() + Q;
         // ----------------------------------------------------------------------- //
 
         setState(state);
